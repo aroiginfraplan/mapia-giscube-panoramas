a7daa52e6238a3684da6c91021cf0c8b219db59c
aroiginfraplan <59873981+aroiginfraplan@users.noreply.github.com>
2021-04-12 13:11:03 +0200

    Panoramas from object storage

--- mapia_panoramas/src/mapia_panoramas/views.py
+++ mapia_panoramas/src/mapia_panoramas/views.py
@@ -3,33 +3,31 @@ import os
 import stat
 
 from django.core.exceptions import PermissionDenied
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404, HttpResponseNotModified
-from django.utils.decorators import method_decorator
 from django.utils.http import http_date
-from django.views.decorators.clickjacking import xframe_options_exempt
 from django.views.static import was_modified_since
 
+import boto3
+from botocore.client import Config
 from oauth2_provider.decorators import protected_resource
 
-
-from .settings import PANORAMAS_ROOT
-
-
-xframe_options_exempt_m = method_decorator(xframe_options_exempt, name='dispatch')
+from .settings import (AWS_STORAGE_BUCKET_NAME, AWS_S3_REGION_NAME, AWS_S3_ENDPOINT_URL,
+                       AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, PANORAMAS_ROOT)
 
 
 @protected_resource()
-@xframe_options_exempt_m
 def panoramas_files_server(request, code, path):
     if not request.user.is_authenticated:
         raise PermissionDenied
 
     path = os.path.join(code, path)
     fullpath = os.path.join(PANORAMAS_ROOT, path)
-    if not os.path.exists(fullpath) or \
-            not os.path.realpath(fullpath).startswith(os.path.join(PANORAMAS_ROOT, code)):
-        raise Http404('"{0}" does not exist'.format(path))
+    if not os.path.exists(fullpath) or not os.path.realpath(fullpath).startswith(os.path.join(PANORAMAS_ROOT, code)):
+        if AWS_STORAGE_BUCKET_NAME:
+            return panoramas_files_s3(request, path)
+        else:
+            raise Http404('"{0}" does not exist'.format(path))
     # Respect the If-Modified-Since header.
     statobj = os.stat(fullpath)
     content_type = mimetypes.guess_type(
@@ -43,3 +41,23 @@ def panoramas_files_server(request, code, path):
     # filename = os.path.basename(path)
     # response['Content-Disposition'] = smart_str(u'attachment; filename={0}'.format(filename))
     return response
+
+
+def panoramas_files_s3(request, path):
+    s3 = boto3.client(
+        service_name='s3',
+        region_name=AWS_S3_REGION_NAME,
+        endpoint_url=AWS_S3_ENDPOINT_URL,
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        config=Config(s3={'addressing_style': 'virtual'})
+    )
+    url = s3.generate_presigned_url(
+        ClientMethod="get_object",
+        ExpiresIn=3600,
+        Params={
+            "Bucket": AWS_STORAGE_BUCKET_NAME,
+            "Key": path,
+        },
+    )
+    return HttpResponseRedirect(url)
